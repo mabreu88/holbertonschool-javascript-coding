c78614c690455769f37d23f18e0e7872f0d98b2e
Martin <martymalkav7@gmail.com>
2024-01-22 11:19:56 -0800

    1. Write me

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+//script that writes a string to a file.
+
+const argvs = process.argv.slice(2);
+const filename = argvs[0];
+const content = argvs[1];
+
+const fs = require('fs');
+fs.writeFile(filename, content, err => {
+  if (err) {
+    console.error(err);
+  }
+});
