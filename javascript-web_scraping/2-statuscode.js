0ceb9d2aa696c4476873210c41a01629a9f6e147
Martin <martymalkav7@gmail.com>
2024-01-22 18:50:19 -0800

    2. Status code

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// script that display the status code of a GET request.
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, function (error, response) {
+  if (error != null) {
+    console.error('error:', error);
+    return;
+  }
+  console.log('code:', response && response.statusCode);
+});
